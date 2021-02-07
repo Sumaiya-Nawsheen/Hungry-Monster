7b9f52bd5c29fd9f1eb4ee392b3d36f835f77f26
Sumaiya-Nawsheen <sumaiya.naw252@gmail.com>
2021-02-07 16:23:30 +0600

    half-done

--- hungryMonster.js
+++ hungryMonster.js
@@ -1,17 +1,21 @@
 fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b')
     .then(res => res.json())
-    .then(data => {
-
-            const items = data.meals;
-            for (let i = 0; i < items.length; i++) {
-                console.log(items[i].strMeal);
-                // const element = array[i];
-
-            }
-        }
-
-        // console.log(data.meals[0].strMeal)
+    .then(data => mealItems(data));
 
 
+const mealItems = diffMeals => {
+    const div = document.getElementById("meals");
+    const items = diffMeals.meals;
+    items.forEach(mealItem => {
+        const newDiv = document.createElement("div");
+        newDiv.className = "mealsDiv";
+        const mealMenu = `
+            <img class="img-style" src=" ${mealItem.strMealThumb} "></img>
+            <br>
+            <h5 class="meal-name"> ${mealItem.strMeal}</h5>
+            `;
+        newDiv.innerHTML = mealMenu;
+        div.appendChild(newDiv);
 
-    );
+    });
+}
